393eb4004c37b21513bcbbd6bfa746b90e0fd1e9
kaaiik <enrperlop@gmail.com>
2022-05-12 13:55:12 +0200

    visualizaciones de sea

--- src/func_aux.py
+++ src/func_aux.py
@@ -8,11 +8,14 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from fbprophet import Prophet
 
+from matplotlib.animation import FuncAnimation
+
 #-----------------------------------------------------------------
 
 def comparador(df_real, df_pred, referencia):
     plt.plot(df_real)
     plt.plot(df_pred)
+    plt.legend(['Realidad', 'Predicción'])
     plt.ylim((min(referencia), max(referencia)));
     
 def train_and_predict(df):
@@ -30,5 +33,22 @@ def train_and_predict(df):
     df_pred = forecast[['ds', 'yhat']].tail(120)
     df_pred = df_pred.set_index(df_pred.ds)
     return df_pred
+    
+def train_and_predict_till2050(df):
+    # renombramos el df para que lo pueda leer bien el modelo
+    ts = df.rename(columns={'Time':'ds', 'GMSL':'y','GMSL uncertainty':'yhat'})
+
+    ts.columns=['ds','y','yhat']
+    model1 = Prophet(yearly_seasonality=True)
+    model1.fit(ts);
+    
+    # predicción de la próxima década
+    future = model1.make_future_dataframe(periods = 361+72, freq = 'M')  
+    # now lets make the forecasts
+    forecast = model1.predict(future)
+    df_pred = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(360+72)
+    df_pred = df_pred.set_index(df_pred.ds)
+    return df_pred
+    
 
 
